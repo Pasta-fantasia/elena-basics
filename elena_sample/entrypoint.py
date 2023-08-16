2b6a0b345907371dcba89345c10f27c72d9c3c99
Fran Simó <fransimo@gmail.com>
2023-08-16 13:38:44 +0200

    clean up

--- elena_sample/entrypoint.py
+++ elena_sample/entrypoint.py
@@ -2,15 +2,13 @@ from elena.config import dependency_injection
 from elena.adapters.config.local_config_reader import LocalConfigReader
 from elena import __version__ as version
 
-from elena_sample.strategy_trailing_stop_with_bb.trailing_stop_bb import TrailingStopLossBBbuyAfterSleep
+
 
 ### for testing only!!!!
 
 def main():
     # debug using ELENA_HOME pointing to ./local_data
     print('Hello World!')
-    x = TrailingStopLossBBbuyAfterSleep()
-    print(x)
 
     config = LocalConfigReader().config
     container = dependency_injection.get_container(config)
