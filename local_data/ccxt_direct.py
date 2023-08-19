257cbe3451be9f950f6bdcf54b30c15443564b93
Fran Simó <fransimo@gmail.com>
2023-08-19 18:48:30 +0200

    "manual" exchange operation

--- local_data/ccxt_direct.py
+++ local_data/ccxt_direct.py
@@ -1,9 +1,13 @@
+# to operate directly with the exchange
+from decouple import AutoConfig
+from decouple import config
 import ccxt
 
-# to operate directly over the exchange
 
-apiKey = ''
-secret = ''
+config = AutoConfig(' ') # https://github.com/henriquebastos/python-decouple/issues/116
+
+apiKey = config('apiKey')
+secret = config('secret')
 
 exchange = ccxt.binance({
     'apiKey': apiKey,
@@ -16,17 +20,20 @@ exchange = ccxt.binance({
 exchange.set_sandbox_mode(True)  # enable sandbox mode
 
 symbol = 'BTC/USDT'
+balance = exchange.fetch_balance()
 market = exchange.market(symbol)
 
-exchange.cancel_all_orders(symbol)
+open_orders = exchange.fetch_open_orders(symbol)
 
-balance = exchange.fetch_balance()
+# exchange.cancel_all_orders(symbol)
+# o22 = exchange.fetch_order('6836222',symbol)
+
 balance_USDT = balance[market['quote']]
 
+# buy 1 BTC, it's needed when the stop orders are triggered
 type = 'market'
 side = 'buy'
 amount = 1
-
 
 exchange.verbose = True
 create_order = exchange.create_order(symbol, type, side, amount)
