3f6e50ac21afda3fcecd68147395c3d20effd5f3
Fran Simó <fransimo@gmail.com>
2023-12-10 22:12:19 +0100

    DCA_Strict

---
+++ elena_basic/strategies/dca_strict.py
@@ -0,0 +1,75 @@
+import pathlib
+from logging import Logger
+
+from elena.domain.model.time_frame import TimeFrame
+from elena.domain.model.bot_config import BotConfig
+from elena.domain.model.trade import Trade
+from elena.domain.model.bot_status import BotStatus
+from elena.domain.ports.exchange_manager import ExchangeManager
+
+from elena.domain.ports.logger import Logger
+from elena.domain.ports.strategy_manager import StrategyManager
+
+from elena.domain.services.elena import Elena
+from elena.domain.services.generic_bot import GenericBot
+
+from elena.adapters.bot_manager.local_bot_manager import LocalBotManager
+from elena.adapters.config.local_config_reader import LocalConfigReader
+from elena.adapters.logger.local_logger import LocalLogger
+
+import pandas_ta as ta
+
+
+class DCA_Strict(GenericBot):
+    # Strict dates DCA, just buy on a regular basis.
+
+    budget: float
+
+    def init(self, manager: StrategyManager, logger: Logger,
+             exchange_manager: ExchangeManager, bot_config: BotConfig, bot_status: BotStatus):
+
+        super().init(manager, logger, exchange_manager, bot_config, bot_status)
+
+        try:
+            self.budget = bot_config.config['budget']
+        except Exception as err:
+            self._logger.error(f"Error initializing Bot config: {err}", error=err)
+
+    def next(self) -> BotStatus:
+        self._logger.info('%s strategy: processing next cycle ...', self.name)
+
+        min_amount = self.limit_min_amount()
+        if not min_amount:
+            raise Exception("Cannot get min_amount")
+
+        estimated_close_price = self.get_estimated_last_close()
+        if not estimated_close_price:
+            raise Exception("Cannot get_estimated_last_close")
+
+        balance = self.get_balance()
+        if not balance:
+            raise Exception("Cannot get balance")
+
+        quote_symbol = self.pair.quote
+        quote_free = balance.currencies[quote_symbol].free
+
+        if self.budget <= quote_free:
+            amount_to_spend = self.budget
+        else:
+            amount_to_spend = quote_free
+        amount_to_buy = amount_to_spend / estimated_close_price
+        amount_to_buy = self.amount_to_precision(amount_to_buy)
+
+        if amount_to_buy < min_amount:
+            self._logger.error("Not enough balance to buy min_amount. {self.pair.base}, quote_free={quote_free}, "
+                               "min_amount={min_amount}, estimated_close_price={estimated_close_price}")
+            raise Exception("Not enough balance to buy min_amount. {self.pair.base}, quote_free={quote_free}, "
+                            "min_amount={min_amount}, estimated_close_price={estimated_close_price}")
+
+        market_buy_order = self.create_market_buy_order(amount_to_buy)
+
+        if not market_buy_order:
+            self._logger.error("Buy order failed!")
+            raise Exception("Buy order failed!")
+
+        return self.status
